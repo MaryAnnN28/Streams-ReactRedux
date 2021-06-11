c32e1ca89a32cebc596cb987d3d9118e71cfcf94
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-06-10 20:51:17 -0400

    added stream reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk'; 
 
 import App from './components/App';
 import reducers from './reducers';
@@ -12,7 +12,7 @@ const composeEnhancers =
 
 const store = createStore(
 	reducers,
-	composeEnhancers(applyMiddleware()),
+	composeEnhancers(applyMiddleware(reduxThunk)),
 );
 
 ReactDOM.render(
