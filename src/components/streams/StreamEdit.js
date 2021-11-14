0d34b17f2805396c5ad08b8fd9c6a1eaa310a9aa
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-11-14 14:16:46 -0500

    Added modal for stream delete, update to patch from put in stream edit

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import React from 'react'
+import { connect } from 'react-redux'
+import formValues from 'redux-form/lib/formValues'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
 
 class StreamEdit extends React.Component {
 	componentDidMount() {
-		this.props.fetchStream(this.props.match.params.id);
+		this.props.fetchStream(this.props.match.params.id)
 	}
+
+	onSubmit = (formValues) => {
+		this.props.editStream(this.props.match.params.id, formValues)
+	}
+
 	render() {
 		if (!this.props.stream) {
-			return <div>Loading...</div>;
+			return <div>Loading...</div>
 		}
 
-		return <div>{this.props.stream.title}</div>;
+		return (
+			<div>
+				<h3>Edit a Stream</h3>
+				<StreamForm initialValues={this.props.stream} onSubmit={this.onSubmit} />
+			</div>
+		)
 	}
 }
 
 const mapStateToProps = (state, ownProps) => {
-	return { stream: state.streams[ownProps.match.params.id] };
-};
+	return { stream: state.streams[ownProps.match.params.id] }
+}
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
